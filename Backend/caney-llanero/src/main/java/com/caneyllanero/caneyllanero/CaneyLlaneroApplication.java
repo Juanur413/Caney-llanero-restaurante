f2f85635e72ecf8df10848f6837a5348b7f138c3
Cristian <cristianmape@live.com>
2022-09-04 16:05:13 -0500

    Inicio del backend configuracion basica

---
+++ Backend/caney-llanero/src/main/java/com/caneyllanero/caneyllanero/CaneyLlaneroApplication.java
@@ -0,0 +1,13 @@
+package com.caneyllanero.caneyllanero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaneyLlaneroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaneyLlaneroApplication.class, args);
+	}
+
+}
---
+++ Backend/caney-llanero/src/test/java/com/caneyllanero/caneyllanero/CaneyLlaneroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caneyllanero.caneyllanero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaneyLlaneroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
